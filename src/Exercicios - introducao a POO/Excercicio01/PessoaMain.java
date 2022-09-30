d7befb7dc2eea69a3ea0ec5b055d067df5ddc340
Anderson Viana <anderson1565@live.com>
2022-09-30 14:32:37 -0300

    insert new exc

--- src/Exercicios - introducao a POO/Excercicio01/PessoaMain.java
+++
@@ -1,8 +0,0 @@
-package Excercicio01;
-
-public class PessoaMain {
-    public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("Anderson", "Viana");
-        System.out.println(pessoa.nomeCompleto());
-    }
-}
