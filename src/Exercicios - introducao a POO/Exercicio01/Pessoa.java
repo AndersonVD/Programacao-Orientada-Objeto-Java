38371a045a04409d0e3186bee4493f88ca2004d0
Anderson Viana <anderson1565@live.com>
2022-09-30 14:29:05 -0300

    insert pdf docs

---
+++ src/Exercicios - introducao a POO/Exercicio01/Pessoa.java
@@ -0,0 +1,40 @@
+package Exercicio01;
+// Faça um programa que peça ao usuário nome e sobrenome separadamente
+
+// e após junte-os com a mensagem “Seu nome completo é: Nome
+// SobreNome”;
+
+public class Pessoa {
+
+    private String nome;
+    private String sobrenome;
+
+    public Pessoa(String nome, String sobrenome) {
+        setNome(nome);
+        setSobrenome(sobrenome);
+    }
+
+    public void setNome(String nome) {
+        if (nome.length() > 0)
+            ;
+        this.nome = nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        if (sobrenome.length() > 0)
+            ;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String nomeCompleto() {
+        return nome + " " + sobrenome;
+    }
+}
---
+++ src/Exercicios - introducao a POO/Exercicio01/PessoaMain.java
@@ -0,0 +1,8 @@
+package Exercicio01;
+
+public class PessoaMain {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa("Anderson", "Viana");
+        System.out.println(pessoa.nomeCompleto());
+    }
+}
